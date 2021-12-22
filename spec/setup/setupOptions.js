8561c87c745802b0b4f8f57f61b6bc6a68ff0e81
Krzysztof Bieroń <7493083+el-dot@users.noreply.github.com>
2021-12-22 11:08:50 +0100

    Change options.url to options.apiUrl

--- spec/setup/setupOptions.js
+++ spec/setup/setupOptions.js
@@ -1,6 +1,9 @@
 const SecurionPayGateway = require('../../')
-if (process.env.URL) {
-  SecurionPayGateway.defaultOptions.url = process.env.URL
+if (process.env.API_URL) {
+  SecurionPayGateway.defaultOptions.apiUrl = process.env.API_URL
+}
+if (process.env.UPLOADS_URL) {
+  SecurionPayGateway.defaultOptions.uploadsUrl = process.env.UPLOADS_URL
 }
 if (process.env.SECRET_KEY) {
   SecurionPayGateway.defaultOptions.secretKey = process.env.SECRET_KEY
